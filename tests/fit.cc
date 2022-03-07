e6e52786ab948118534cc884eed3f3a50df5e772
Xuefeng, Ding <xuefeng.ding.physics@gmail.com>
2022-03-07 16:18:02 -0500

    add sanitizers; fix memory leak in fit.cc; add my.supp

--- tests/fit.cc
+++ tests/fit.cc
@@ -1,4 +1,5 @@
 #include "fit.h"
+
 #include "TRandom.h"
 #include "goofit/BinnedDataSet.h"
 #include "goofit/FitManager.h"
@@ -7,24 +8,24 @@
 #include "goofit/Variable.h"
 namespace GooFit {
   void fit(double &a, double &b) {
-    auto *energy = new Variable("energy", 1, 100);  // nbins = 100
-    auto *data = new BinnedDataSet(energy);
+    auto energy = new Variable("energy", 1, 100);  // nbins = 100
+    auto data = std::make_shared<BinnedDataSet>(energy);
     gRandom->SetSeed(1);
     for (int i = 0; i < 100; ++i) {
       data->setBinContent(i, gRandom->Poisson(1000 * exp(-(i + 0.5) / 370)));
     }
-    auto *lambda = new Variable("lambda", -1. / 400, -1. / 40, -1. / 4000);
+    auto lambda = new Variable("lambda", -1. / 400, -1. / 40, -1. / 4000);
     GooPdf *pdf = new ExpPdf("exp", energy, lambda);
     std::vector<Variable *> Ns;
     Ns.push_back(new Variable("N", 1000, 1, 0, 10000));
     std::vector<PdfBase *> pdfs;
     pdfs.push_back(pdf);
-    auto *likelihood = new SumPdf("likelihood", 1, Ns, pdfs, energy);
-    likelihood->setData(data);
+    auto likelihood = new SumPdf("likelihood", 1, Ns, pdfs, energy);
+    likelihood->setData(data.get());
 
-    auto *fit = new FitManager(likelihood);
-    fit->fit();
-    fit->getMinuitValues();
+    FitManager fit(likelihood);
+    fit.fit();
+    fit.getMinuitValues();
 
     a = lambda->value;
     b = Ns.front()->value;
