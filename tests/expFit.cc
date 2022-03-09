ad4cb13a9627ba1e7b80b9759cb28cc5d4ccc3f7
Xuefeng, Ding <xuefeng.ding.physics@gmail.com>
2022-03-09 14:13:53 -0500

    bug fix

--- tests/expFit.cc
+++ tests/expFit.cc
@@ -7,6 +7,4 @@ TEST(GooFit, testFitResult) {
   GooFit::fit(a, b);
   EXPECT_NEAR(-1. / 370, a, 1. / 370 * 2e-2);
   EXPECT_NEAR(1000, b, 1000 * 2e-2);
-  int c[3];
-  std::cout << c[3] << std::endl;  // ASan
 }
